3bfb1aa0fa7f087536f5549a6c37f0268141df50
dooburt <dooburt@gmail.com>
2024-07-01 14:46:34 +0100

    fixed not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import village from "../../../public/stock/village.jpg";
-import DiscordInvite from "../components/DiscordInvite/DiscordInvite";
-import PageHead from "../components/PageHead/PageHead";
+import village from "../../public/stock/village.jpg";
+import DiscordInvite from "./components/DiscordInvite/DiscordInvite";
+import PageHead from "./components/PageHead/PageHead";
 
 export default function NotFound({ params, searchParams }) {
   return (
@@ -13,7 +13,11 @@ export default function NotFound({ params, searchParams }) {
       <section className="py-8 bg-purple-900">
         <div className="container px-8 xl:px-0 mx-auto">
           <p className="font-body py-8 text-base lg:text-2xl leading-relaxed text-white">
-            Unfortunately, we can't find that 🫤. Sorry! Try our <Link href="/">home page</Link>.
+            Unfortunately, we can't find that 🫤. Sorry! Try our{" "}
+            <Link href="/" className="text-purple-200">
+              home page
+            </Link>
+            .
           </p>
         </div>
       </section>
